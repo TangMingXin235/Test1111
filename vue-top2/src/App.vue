3c82ba7fbc5f96ba98e6947f6a82fd20dadb077d
Wzz12311 <2639662505@qq.com>
2024-09-03 20:49:41 +0800

    VUe

---
+++ vue-top2/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o"  replace to="/" >首页</van-tabbar-item>
+      <van-tabbar-item icon="search" replace to="/category">分类</van-tabbar-item>
+      <van-tabbar-item icon="friends-o" replace to="/user">个人</van-tabbar-item>
+    </van-tabbar>
+   
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        active:0
+      }
+
+    }
+
+  }
+</script>
---
+++ vue-top2/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'vant/lib/index.css';
+
+import { Button } from 'vant';
+import { Tabbar, TabbarItem } from 'vant';
+
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
+Vue.use(Button);
+
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
